ee7192c17044b3b7a4b54f870098631063efcf8d
lijiongjiong <lijiongjiong163@163.com>
2020-05-14 20:13:49 +0800

    object方法实现线程通讯

---
+++ thread_com_demo/src/main/java/com/ljj/thread/OddEvenDemo.java
@@ -0,0 +1,57 @@
+package com.ljj.thread;
+
+/**
+ * 线程通讯：
+ * 使用Object的方法来实现线程间的通信
+ * 实现效果：
+ * 线程一打印奇数，线程二打印偶数
+ */
+public class OddEvenDemo {
+    private int i = 0;
+    private  Object object = new Object();
+    /**
+     * 奇数打印方法
+     */
+    public  void odd(){
+        synchronized (object){
+            while (i<10)
+            {
+                if (i%2==1){
+                    System.out.println("线程"+Thread.currentThread().getName()+"打印奇数："+i);
+                    i++;
+                    object.notify();//唤醒奇数线程打印
+                }else {
+                    try {
+                        object.wait();//等待偶数线程打印完毕
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+
+        }
+      }
+    public synchronized void even(){
+synchronized (object){
+
+    while (i<10)
+    {
+        if (i%2==0){
+            System.out.println("线程"+Thread.currentThread().getName()+"打印偶数："+i);
+            i++;
+            object.notify();//唤醒偶数线程打印
+        }else {
+            try {
+                object.wait();//等待奇数线程打印完毕
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+
+}}
+
+}
---
+++ thread_com_demo/src/test/java/com/ljj/thread/OddEvenDemoTest.java
@@ -0,0 +1,26 @@
+package com.ljj.thread;
+
+
+/**
+ * 1.object.notify()或者object.wait(),都是让当前操作这个对象（或者说执行这个代码）的对象等待或者去唤醒别人
+ * 2.这两个玩意必须放在synchronized (object){}代码块里，不然会报IllegalMonitorStateException
+ * 3.要在内部类中使用局部变量，局部变量必须被final修饰
+ */
+class OddEvenDemoTest {
+
+    public static void main(String[] args) {
+       final OddEvenDemo oddEvenDemo = new OddEvenDemo();
+        Thread thread1 = new Thread(new Runnable() {
+            public void run() {
+                oddEvenDemo.odd();
+            }
+        });
+        Thread thread2 = new Thread(new Runnable() {
+            public void run() {
+                oddEvenDemo.even();
+            }
+        });
+        thread1.start();
+        thread2.start();
+    }
+}
