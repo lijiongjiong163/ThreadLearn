61ce00e74d256344c90233ce4800554c4d79b067
lijiongjiong <lijiongjiong163@163.com>
2020-05-14 15:01:54 +0800

    线程不安全例子

--- thread_safe_demo/src/main/java/com/ljj/thread/Ticket.java
+++ thread_safe_demo/src/main/java/com/ljj/thread/Ticket.java
@@ -1,29 +1,78 @@
 package com.ljj.thread;
 
 import java.util.Date;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
-public class Ticket implements Runnable{
+public class Ticket implements Runnable {
 
     private int ticketNum = 100;
+    private Object object = new Object();
+    //3.1先创建lock接口实现类对象
+    private Lock lock = new ReentrantLock(true);//传参决定是否是公平锁：true--公平锁，多个线程都公平拥有执行权；false--非公平锁（默认），独占锁
 
     @Override
     public void run() {
 
-            while(true){
-                //是否有票
-                if (ticketNum>0){
-                    //有的话办手续卖票
-                    try {
-                        Thread.sleep(100);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
+            while (true) {
+                //1.synchronized (object)方法
+                /*synchronized (object) {
+                    //是否有票
+                    if (ticketNum > 0) {
+                        //有的话办手续卖票
+                        try {
+                            Thread.sleep(100);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        //打印出线程名和当前票数，然后票数减一结束本次卖票
+                        String name = Thread.currentThread().getName();
+                        System.out.println("线程" + name + "销售电影票：" + ticketNum--);
                     }
-                    //打印出线程名和当前票数，然后票数减一结束本次卖票
-                    String name = Thread.currentThread().getName();
-                    System.out.println("线程"+name+"销售电影票："+ticketNum--);
+                }*/
+                //2.使用同步方法
+                //saleticket();//2.2使用这方法就完事了
+                //3.使用同步锁的方法
+                //3.2加锁
+                lock.lock();
+                try {
+                    //写可能产生线程安全问题的代码
+                    if (ticketNum > 0) {
+                        try {
+                            Thread.sleep(100);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        String name = Thread.currentThread().getName();
+                        System.out.println("线程" + name + "销售电影票：" + ticketNum--);
+                    }
+                }finally {
+                    //3.3解锁
+                    //由于不调unlock方法会引起死锁，所以防止程序出错而执行不到unlock，使用try块包住，然后使用finally中解锁，这样就一定能解锁了
+                    lock.unlock();
                 }
 
 
-        }
+            }
+
+    }
+
+    /**
+     * 2.1同步方法
+     */
+    public synchronized void saleticket(){
+            //是否有票
+            if (ticketNum > 0) {
+                //有的话办手续卖票
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                //打印出线程名和当前票数，然后票数减一结束本次卖票
+                String name = Thread.currentThread().getName();
+                System.out.println("线程" + name + "销售电影票：" + ticketNum--);
+            }
+
     }
 }
--- thread_safe_demo/src/test/java/com/ljj/thread/TicketTest.java
+++ thread_safe_demo/src/test/java/com/ljj/thread/TicketTest.java
@@ -13,8 +13,10 @@ public class TicketTest {
         Ticket ticket = new Ticket();
         Thread thread1 = new Thread(ticket,"窗口1");
         Thread thread2 = new Thread(ticket,"窗口2");
+        Thread thread3 = new Thread(ticket,"窗口3");
         thread1.start();
         thread2.start();
+        thread3.start();
         System.out.println("什么鬼");
     }
 }
